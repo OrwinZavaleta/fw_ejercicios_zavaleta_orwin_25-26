3e9d23eb1ead4c0a229f1a3daf93506fc0a01206
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-01-30 14:20:23 +0100

    formularios reactivos

---
+++ fw_t02p02_zavaleta_orwin/mi_app_03/src/app/details/details.ts
@@ -0,0 +1,35 @@
+import { Housing } from './../services/housing';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingLocationInfo } from '../interfaces/housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-details',
+  imports: [ReactiveFormsModule],
+  templateUrl: './details.html',
+  styleUrl: './details.css',
+})
+export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(Housing);
+  housingLocation: HousingLocationInfo | undefined;
+
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
+  }
+}
---
+++ fw_t02p02_zavaleta_orwin/mi_app_03/src/app/home/home.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationInfo } from '../interfaces/housinglocation';
+import { Housing } from '../services/housing';
+
+@Component({
+  selector: 'app-home',
+  imports: [HousingLocation],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  housingLocationList: HousingLocationInfo[] = [];
+  filteredLocationList: HousingLocationInfo[] = [];
+  housingService: Housing = inject(Housing);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
+
+  // housingLocation: HousingLocationInfo = {
+  //   id: 9999,
+  //   name: 'Test Home',
+  //   city: 'Test city',
+  //   state: 'ST',
+  //   photo: `${this.baseUrl}/example-house.jpg`,
+  //   availableUnits: 99,
+  //   wifi: true,
+  //   laundry: false,
+  // };
+}
