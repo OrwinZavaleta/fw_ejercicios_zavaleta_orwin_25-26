bc6e9b5b79e4f6fd1c92ed91a66d5dd537322563
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-01-30 21:15:27 +0100

    login

--- fw_t02p02_zavaleta_orwin/mi_app_03/src/app/app.routes.ts
+++ fw_t02p02_zavaleta_orwin/mi_app_03/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Details } from './details/details';
+import { Login } from './login/login';
 
 export const routes: Routes = [
   {
     path: '',
+    component: Login,
+    title: 'Login',
+  },
+  {
+    path: 'home',
     component: Home,
     title: 'Home page',
   },
