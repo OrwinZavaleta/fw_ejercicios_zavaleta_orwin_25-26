4e8e0f9452189de9d0ac2a894b1b6bcccb815abd
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-01-30 21:21:03 +0100

    Guards, proteccion de las rutas

--- fw_t02p02_zavaleta_orwin/mi_app_03/src/app/app.routes.ts
+++ fw_t02p02_zavaleta_orwin/mi_app_03/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Details } from './details/details';
 import { Login } from './login/login';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -13,10 +14,12 @@ export const routes: Routes = [
     path: 'home',
     component: Home,
     title: 'Home page',
+    canActivate: [authGuard],
   },
   {
     path: 'details/:id',
     component: Details,
     title: 'Home details',
+    canActivate: [authGuard],
   },
 ];
