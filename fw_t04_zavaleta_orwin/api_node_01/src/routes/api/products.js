44ed343e88545de3d5f2793d3a6537edaa4a69a1
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-02-26 12:25:11 +0100

    Correccion en el enrutador de productos

--- fw_t04_zavaleta_orwin/api_node_01/src/routes/api/products.js
+++ fw_t04_zavaleta_orwin/api_node_01/src/routes/api/products.js
@@ -4,9 +4,11 @@ const productsController = require("../../controllers/products.controller");
 
 router.get("/", productsController.getAllProducts);
 router.post("/", productsController.createProduct);
+
+router.get("/search", productsController.searchProducts);
+
 router.put("/:id", productsController.updateProduct);
 router.delete("/:id", productsController.deleteProduct);
 router.get("/:id", productsController.getProductById);
-router.get("/search", productsController.searchProducts);
 
 module.exports = router;
