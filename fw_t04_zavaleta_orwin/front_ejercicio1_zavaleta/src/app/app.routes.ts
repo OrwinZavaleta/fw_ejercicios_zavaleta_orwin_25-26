260ad85527290af2c4ceb7c510bc4bb0efee6825
OrwinZavaleta <orwinzavaleta@gmail.com>
2026-03-05 21:59:41 +0100

    Creación del home y el hero y las rutas a las páginas de characters y episodes

--- fw_t04_zavaleta_orwin/front_ejercicio1_zavaleta/src/app/app.routes.ts
+++ fw_t04_zavaleta_orwin/front_ejercicio1_zavaleta/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Layout } from './c_layout/layout/layout';
+import { Home } from './c_pages/home/home';
+import { Characters } from './c_pages/characters/characters';
+import { Episodes } from './c_pages/episodes/episodes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+      { path: 'characters', component: Characters },
+      { path: 'episodes', component: Episodes },
+    ],
+  },
+];
--- fw_t04_zavaleta_orwin/front_ejercicio1_zavaleta/src/app/app.ts
+++ fw_t04_zavaleta_orwin/front_ejercicio1_zavaleta/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front_ejercicio1_zavaleta');
+  protected readonly title = signal('FW Series');
 }
